39a3eb1f4399e512d791c5092bda1f813e51226f
Alex Eremia <alexandrueremia@nyanheroes.com>
2025-04-29 15:43:19 +0300

    chore(cleanup): remove tracked frontend/src/shared-constants/ (now ignored by .gitignore)

--- frontend/src/shared-constants/index.d.ts
+++
@@ -1,6 +0,0 @@
-// Acest fișier ajută TypeScript să recunoască shared-constants ca modul global.
-export * from './enums';
-export * from './defaults';
-export * from './messages';
-export * from './ui';
-export * from './categories';
