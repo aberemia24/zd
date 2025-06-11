a7a0fa113055d1496ff287a62cd5038995fec097
buzunarul <aberemia@gmail.com>
2025-06-11 21:00:23 +0300

    add ultra-strict TypeScript and ESLint configuration - prevents AI from using wrong functions/params

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,9 @@ module.exports = {
     node: true
   },
   extends: [
-    'eslint:recommended'
+    'eslint:recommended',
+    '@typescript-eslint/recommended',
+    '@typescript-eslint/recommended-requiring-type-checking'
   ],
   plugins: [
     '@typescript-eslint',
@@ -35,25 +37,46 @@ module.exports = {
           jsx: true
         },
         ecmaVersion: 'latest',
-        sourceType: 'module'
+        sourceType: 'module',
+        project: ['./tsconfig.json', './frontend/tsconfig.json', './backend/tsconfig.json', './shared-constants/tsconfig.json']
       },
       rules: {
-        // TypeScript rules
-        '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
-        '@typescript-eslint/no-explicit-any': 'warn',
-        
+        // TypeScript rules - FOARTE STRICTE
+        '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+        '@typescript-eslint/no-explicit-any': 'error',
+        '@typescript-eslint/no-unsafe-assignment': 'error',
+        '@typescript-eslint/no-unsafe-call': 'error',
+        '@typescript-eslint/no-unsafe-member-access': 'error',
+        '@typescript-eslint/no-unsafe-return': 'error',
+        '@typescript-eslint/no-unsafe-argument': 'error',
+        '@typescript-eslint/strict-boolean-expressions': 'error',
+        '@typescript-eslint/prefer-nullish-coalescing': 'error',
+        '@typescript-eslint/prefer-optional-chain': 'error',
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        '@typescript-eslint/no-floating-promises': 'error',
+        '@typescript-eslint/await-thenable': 'error',
+        '@typescript-eslint/require-await': 'error',
+        '@typescript-eslint/no-misused-promises': 'error',
+
         // React rules
         'react/react-in-jsx-scope': 'off',
         'react/prop-types': 'off',
-        
+
         // React Hooks rules
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
-        
-        // General rules
+
+        // General rules - STRICTE
         'no-console': ['warn', { allow: ['warn', 'error'] }],
-        'prefer-const': 'warn',
-        'no-var': 'error'
+        'prefer-const': 'error',
+        'no-var': 'error',
+        'eqeqeq': ['error', 'always'],
+        'curly': ['error', 'all'],
+        'no-throw-literal': 'error',
+        'no-implicit-coercion': 'error',
+        'no-magic-numbers': ['warn', { ignore: [0, 1, -1, 2] }],
+        'no-duplicate-imports': 'error',
+        'prefer-template': 'error'
       },
       settings: {
         react: {
@@ -80,4 +103,4 @@ module.exports = {
       }
     }
   ]
-}; 
+};
